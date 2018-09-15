588889af2572f4940d098436a20f4a13c9cdfed3
soldier711 <21021736+soldier711@users.noreply.github.com>
2018-09-15 22:55:28 +0200

    Update Client.js

--- structs/Client.js
+++ structs/Client.js
@@ -121,7 +121,9 @@ class Client {
   _initialize () {
     const bot = storage.bot
     if (this.configOverrides && this.configOverrides.setPresence === true) {
-      if (config.bot.activityType) bot.user.setActivity(config.bot.activityName, { type: config.bot.activityType, url: config.bot.streamActivityURL })
+      if (config.bot.activityType) {
+        bot.user.setActivity(config.bot.activityName, { type: config.bot.activityType, url: config.bot.streamActivityURL })
+      }
       else bot.user.setActivity(null)
       bot.user.setStatus(config.bot.status)
     }
