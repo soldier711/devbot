55b9a9c3038ddb894946723bf9edce6357515fce
soldier711 <21021736+soldier711@users.noreply.github.com>
2018-09-16 11:43:41 +0200

    Update commands.js

--- settings/commands.js
+++ settings/commands.js
@@ -1,32 +1,50 @@
 module.exports = function (bot, message) {
   if (message.content === '-mycustomcommand') message.channel.send('I saw your custom command!')
-  if (message.content === '-messageembed') message.channel.send({embed: {
-    color: 3447003,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
+  if (message.content === '-messageembed') message.channel.send({embed = {
+  "title": "title ~~(did you know you can have markdown here too?)~~",
+  "description": "this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```",
+  "url": "https://discordapp.com",
+  "color": 7159546,
+  "timestamp": "2018-09-16T09:40:37.473Z",
+  "footer": {
+    "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
+    "text": "footer text"
+  },
+  "thumbnail": {
+    "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+  },
+  "image": {
+    "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+  },
+  "author": {
+    "name": "author name",
+    "url": "https://discordapp.com",
+    "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+  },
+  "fields": [
+    {
+      "name": "🤔",
+      "value": "some of these properties have certain limits..."
     },
-    title: "This is an embed",
-    url: "http://google.com",
-    description: "This is a test embed to showcase what they look like and what they can do.",
-    fields: [{
-        name: "Fields",
-        value: "They can have different fields with small headlines."
-      },
-      {
-        name: "Masked links",
-        value: "You can put [masked links](http://google.com) inside of rich embeds."
-      },
-      {
-        name: "Markdown",
-        value: "You can put all the *usual* **__Markdown__** inside of them."
-      }
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "© Example"
+    {
+      "name": "😱",
+      "value": "try exceeding some of them!"
+    },
+    {
+      "name": "🙄",
+      "value": "an informative error should show up, and this view will remain as-is until all issues are fixed"
+    },
+    {
+      "name": "<:thonkang:219069250692841473>",
+      "value": "these last two",
+      "inline": true
+    },
+    {
+      "name": "<:thonkang:219069250692841473>",
+      "value": "are inline fields",
+      "inline": true
     }
-  }
-})
+  ]
+};
+channel.send("this `supports` __a__ **subset** *of* ~~markdown~~ 😃 ```js\nfunction foo(bar) {\n  console.log(bar);\n}\n\nfoo(1);```", { embed });
 }
