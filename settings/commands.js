7f183d2870c24896511d388dc866cdff0d206dbf
soldier711 <21021736+soldier711@users.noreply.github.com>
2018-09-16 11:05:39 +0200

    Update commands.js

--- settings/commands.js
+++ settings/commands.js
@@ -1,3 +1,5 @@
 module.exports = function (bot, message) {
   if (message.content === '-mycustomcommand') message.channel.send('I saw your custom command!')
 }
+  if (message.content === '-messageembed') message.channel.send('I want to send a embed message!')
+}
