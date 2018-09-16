a8088955241c1f7dd597abbde1e1f9465555b9c1
soldier711 <21021736+soldier711@users.noreply.github.com>
2018-09-16 11:36:37 +0200

    Update commands.js

--- settings/commands.js
+++ settings/commands.js
@@ -1,13 +1,13 @@
 module.exports = function (bot, message) {
   if (message.content === '-mycustomcommand') message.channel.send('I saw your custom command!')
   if (message.content === '-messageembed') 
-		const embed = new Discord.RichEmbed()
-		.setColor(0x954D23)
-		.setTitle("Command List:")
-		.addField("!help", "Will give the current command list")
-		.addField("!ping", "WIll show the ping time for the bot")
-		.addField("!say [text]", "Will make the bot say something")
-		.addField("!announcement [text]", "Will make the bot say an announcement and tag everyone")
-		.addField("!cat", "Will send a random cat image");
-		message.channel.send({embed})
+	const embed = new Discord.RichEmbed()
+	.setColor(0x954D23)
+	.setTitle("Command List:")
+	.addField("!help", "Will give the current command list")
+	.addField("!ping", "WIll show the ping time for the bot")
+	.addField("!say [text]", "Will make the bot say something")
+	.addField("!announcement [text]", "Will make the bot say an announcement and tag everyone")
+	.addField("!cat", "Will send a random cat image");
+	message.channel.send({embed})
 }
