be74af3e2641284699c91ee5d748622a9bec6331
soldier711 <21021736+soldier711@users.noreply.github.com>
2018-09-16 11:38:58 +0200

    Update commands.js

--- settings/commands.js
+++ settings/commands.js
@@ -1,13 +1,32 @@
 module.exports = function (bot, message) {
   if (message.content === '-mycustomcommand') message.channel.send('I saw your custom command!')
-  if (message.content === '-messageembed') 
-	const embed = new Discord.RichEmbed()
-	.setColor(0x954D23)
-	.setTitle("Command List:")
-	.addField("!help", "Will give the current command list")
-	.addField("!ping", "WIll show the ping time for the bot")
-	.addField("!say [text]", "Will make the bot say something")
-	.addField("!announcement [text]", "Will make the bot say an announcement and tag everyone")
-	.addField("!cat", "Will send a random cat image");
-	message.channel.send({embed})
+  if (message.content === '-messageembed') message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Example"
+    }
+  }
+})
 }
